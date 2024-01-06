2ff85414cb110a173ec9a489fbd85f09dd123608
chronicer <58031046+chronicer@users.noreply.github.com>
2024-01-06 19:00:56 +0300

    Update cAttentionDisp.h

--- src/game/cAttentionDisp.h
+++ src/game/cAttentionDisp.h
@@ -1,13 +1,1 @@
-#pragma once
 
-class cAttentionDisp
-{
-public:
-    int field_4;
-    int field_8;
-    int field_C;
-    int field_10;
-    int field_14;
-
-    virtual ~cAttentionDisp() {};
-};
